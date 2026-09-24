4a56e84e94e4e8e90a40613ce17733e5bef34af6
DiegoM <villegasmisael15@gmail.com>
2026-09-23 22:13:21 -0600

    NEW

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -10,7 +10,7 @@ class SensorController extends Controller
 {
     public function index()
     {
-        // Consultas separadas por dispositivo para evitar confusiones en la vista
+        // Consultas separadas para alimentar las dos secciones de tu nueva vista
         $registrosElectrovolvulas = Sensor::where('esp32_id', 'ENTRADA ELECTROVALVULAS')->latest()->take(5)->get();
         $registrosInvernadero1 = Sensor::where('esp32_id', 'INVERNADERO 1')->latest()->take(5)->get();
         
@@ -19,22 +19,31 @@ class SensorController extends Controller
 
     public function almacenar(Request $request)
     {
+        // Guardamos los datos mapeando todas las variantes posibles de nombres de llaves
         $id = DB::table('sensores_invernadero')->insertGetId([
-            'esp32_id'          => $request->input('esp32_id', 'INVERNADERO 1'),
-            'temp_ambiente'     => $request->input('temp_aht'),
-            'humedad_ambiente'  => $request->input('hum_aht'),
-            'calidad_aire_eco2' => $request->input('eco2'),
-            'calidad_aire_tvoc' => $request->input('tvoc'),
-            'luz_lux'           => $request->input('lux'),
-            'temp_infrarrojo'   => $request->input('temp_obj'),
+            'esp32_id'          => $request->input('esp32_id', 'ESP32_INVERNADERO_1'),
+            'temp_ambient'      => $request->input('temp_ambient', $request->input('temp_aht')),
+            'humedad_ambiente'  => $request->input('humedad_ambiente', $request->input('hum_aht')),
+            'calidad_aire_eco2' => $request->input('calidad_aire_eco2', $request->input('eco2')),
+            'calidad_aire_tvoc' => $request->input('calidad_aire_tvoc', $request->input('tvoc')),
+            'luz_lux'           => $request->input('luz_lux', $request->input('lux')),
+            'temp_infrarrojo'   => $request->input('temp_infrarrojo', $request->input('temp_obj')),
             'tds_valor'         => $request->input('tds_valor'),
-            'he390_valor'       => $request->input('hw390_porcentaje'),
+            'ads3_a1'           => $request->input('ads3_a1'),
+            'ads3_a2'           => $request->input('ads3_a2'),
+            'ads3_a3'           => $request->input('ads3_a3'),
+            'he390_valor'       => $request->input('he390_valor', $request->input('hw390_porcentaje')),
+            'ads4_a1'           => $request->input('ads4_a1'),
+            'ads4_a2'           => $request->input('ads4_a2'),
+            'ads4_a3'           => $request->input('ads4_a3'),
             'ph_valor'          => $request->input('ph_valor'),
+            'ads5_a1'           => $request->input('ads5_a1'),
+            'ads5_a2'           => $request->input('ads5_a2'),
+            'ads5_a3'           => $request->input('ads5_a3'),
             'temp_ds18b20'      => $request->input('temp_ds18b20'),
             'peso_bascula_1'    => $request->input('peso_bascula_1'),
             'peso_bascula_2'    => $request->input('peso_bascula_2'),
-            'peso_bascula_3'    => $request->input('peso_bascula_3'),
-            'peso_bascula_4'    => $request->input('peso_bascula_4'),
+            'peso_hx711'        => $request->input('peso_hx711', $request->input('peso')),
             'created_at'        => now(),
             'updated_at'        => now(),
         ]);
--- app/Models/Sensor.php
+++ app/Models/Sensor.php
@@ -9,8 +9,10 @@ class Sensor extends Model
 {
     use HasFactory;
 
+    // Nombre real de la tabla en tu base de datos
     protected $table = 'sensores_invernadero';
 
+    // Habilitar la asignación masiva para absolutamente todos los campos de ambos dispositivos
     protected $fillable = [
         'esp32_id',
         'temp_ambiente',
@@ -20,9 +22,19 @@ class Sensor extends Model
         'luz_lux',
         'temp_infrarrojo',
         'tds_valor',
+        'ads3_a1',
+        'ads3_a2',
+        'ads3_a3',
         'he390_valor',
+        'ads4_a1',
+        'ads4_a2',
+        'ads4_a3',
         'ph_valor',
+        'ads5_a1',
+        'ads5_a2',
+        'ads5_a3',
         'temp_ds18b20',
+        'peso_hx711',
         'peso_bascula_1',
         'peso_bascula_2',
         'peso_bascula_3',
